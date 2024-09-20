e56b8748855cd40392e589a39ae673e50c1b97be
Liam Boog <mothercow5@gmail.com>
2024-09-19 21:48:52 -0400

    cancelled jump bug fix

--- Assets/Scripts/JumpingState.cs
+++ Assets/Scripts/JumpingState.cs
@@ -3,25 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public abstract partial class MovementState
 {
-    
+    protected KinematicSegment<float>[] Jump(float t, ref KinematicState<float> kinematics, float gravity)
+    {
+        List<KinematicSegment<float>> output = new();
+        if (kinematics.velocity > 0f)
+        {
+            output.Add(AccelerateTowardTargetVelocity(ref t, 0f, gravity, ref kinematics));
+        }
+        output.AddRange(FallingCurve(t, ref kinematics, player.Aim.x, player.WallCheck));
+        return output.ToArray();
+    }
 }
 
 public class JumpingState : MovementState
 {
     private delegate void JumpInitializer(ref KinematicState<Vector2> kinematics);
     
-    private float initialHeight;
-    private float gravity;
+    private readonly float initialHeight;
     private JumpInitializer onFirstUpdate;
 
     public JumpingState(MovementParameters movementParameters, IPlayerInfo playerInfo, KinematicState<Vector2> initialKinematics) : base(movementParameters, playerInfo)
     {
         initialHeight = initialKinematics.position.y;
         onFirstUpdate = OnFirstUpdate;
-        gravity = parameters.RiseGravity;
         
         void OnFirstUpdate(ref KinematicState<Vector2> kinematics)
         {
@@ -30,18 +38,13 @@ public class JumpingState : MovementState
         }
     }
 
-    private JumpingState(JumpingState previous, float gravity) : base(previous.parameters, previous.player)
-    {
-        this.gravity = gravity;
-    }
-    
     protected override MovementState Update(ref float t, ref KinematicState<Vector2> kinematics, IEnumerable<IInterrupt> interrupts)
     {
         onFirstUpdate?.Invoke(ref kinematics);
 
         if (interrupts.Any(i => i is JumpInterrupt { type: JumpInterrupt.Type.Cancelled }))
         {
-            return new JumpingState(this, GetCancelledJumpGravityMagnitude(kinematics));
+            return new CancelledJumpState(parameters, player,  GetCancelledJumpGravityMagnitude(kinematics));
         }
         if (interrupts.FirstOrDefault(i => i is ICollision) is ICollision collision)
         {
@@ -57,7 +60,7 @@ public class JumpingState : MovementState
         KinematicState<float> xKinematics = new(kinematics.position.x, kinematics.velocity.x);
         KinematicState<float> yKinematics = new(kinematics.position.y, kinematics.velocity.y);
         
-        Jump(t, ref yKinematics);
+        Jump(t, ref yKinematics, parameters.RiseGravity);
         WalkingCurve(t, ref xKinematics, player.Aim.x);
         kinematics = new(
             new(xKinematics.position, yKinematics.position), 
@@ -67,17 +70,6 @@ public class JumpingState : MovementState
         return this;
     }
     
-    protected KinematicSegment<float>[] Jump(float t, ref KinematicState<float> kinematics)
-    {
-        List<KinematicSegment<float>> output = new();
-        if (kinematics.velocity > 0f)
-        {
-            output.Add(AccelerateTowardTargetVelocity(ref t, 0f, gravity, ref kinematics));
-        }
-        output.AddRange(FallingCurve(t, ref kinematics, player.Aim.x, player.WallCheck));
-        return output.ToArray();
-    }
-    
     private float GetCancelledJumpGravityMagnitude(KinematicState<Vector2> kinematics)
     {
         float verticalDisplacement = kinematics.position.y - initialHeight;
@@ -85,5 +77,43 @@ public class JumpingState : MovementState
         float remainingRise = Mathf.Min(maxRise, parameters.MaxJumpHeight - verticalDisplacement);
         
         return 0.5f * kinematics.velocity.y * kinematics.velocity.y / remainingRise;
+    }
+}
+
+public class CancelledJumpState : MovementState
+{
+    private readonly float gravity;
+    
+    public CancelledJumpState(MovementParameters movementParameters, IPlayerInfo playerInfo, float gravity) : base(movementParameters, playerInfo)
+    {
+        parameters = movementParameters;
+        player = playerInfo;
+        this.gravity = gravity;
+    }
+
+    protected override MovementState Update(ref float t, ref KinematicState<Vector2> kinematics, IEnumerable<IInterrupt> interrupts)
+    {
+        if (interrupts.FirstOrDefault(i => i is ICollision) is ICollision collision)
+        {
+            if (Vector2.Dot(collision.Normal, Vector2.up) > 0.5f) // Collision with ground
+            {
+                if (player.JumpBuffer.Flush())
+                    return new JumpingState(parameters, player, kinematics);
+                
+                return new WalkingState(parameters, player);
+            }
+        }
+        
+        KinematicState<float> xKinematics = new(kinematics.position.x, kinematics.velocity.x);
+        KinematicState<float> yKinematics = new(kinematics.position.y, kinematics.velocity.y);
+        
+        Jump(t, ref yKinematics, gravity);
+        WalkingCurve(t, ref xKinematics, player.Aim.x);
+        kinematics = new(
+            new(xKinematics.position, yKinematics.position), 
+            new(xKinematics.velocity, yKinematics.velocity));
+
+        t = 0f;
+        return this;
     }
 }
