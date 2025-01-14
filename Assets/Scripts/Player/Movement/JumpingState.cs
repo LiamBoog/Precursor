17fb5be5c45a065f5e5d26420946f308f4a548be
Liam Boog <mothercow5@gmail.com>
2025-01-14 18:16:58 -0500

    prettify jump animation

--- Assets/Scripts/Player/Movement/JumpingState.cs
+++ Assets/Scripts/Player/Movement/JumpingState.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
 public abstract partial class MovementState
--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -5,15 +5,12 @@ public class PlayerAnimator : MonoBehaviour
     [SerializeField] private PlayerController playerController;
     [SerializeField] private Animator animator;
     [SerializeField] private ExpMovingAverageFloat horizontalVelocity;
-    [SerializeField] private ExpMovingAverageFloat verticalVelocity;
     
     private AnimationState currentState;
 
     private void Update()
     {
-        verticalVelocity.AddSample(playerController.Velocity.y, Time.deltaTime);
-        animator.SetFloat("VerticalVelocity", verticalVelocity);
-        
+        animator.SetFloat("VerticalVelocity", playerController.Velocity.y);
         animator.SetBool("Grounded", playerController.GroundCheck());
         
         horizontalVelocity.AddSample(playerController.Velocity.x, Time.deltaTime);
