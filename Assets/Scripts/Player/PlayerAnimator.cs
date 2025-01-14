0b5454590aba682ef50405dec6346ac8c1c9bcb9
Liam Boog <mothercow5@gmail.com>
2025-01-13 22:47:36 -0500

    some code for jump animations

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -5,11 +5,17 @@ public class PlayerAnimator : MonoBehaviour
     [SerializeField] private PlayerController playerController;
     [SerializeField] private Animator animator;
     [SerializeField] private ExpMovingAverageFloat horizontalVelocity;
+    [SerializeField] private ExpMovingAverageFloat verticalVelocity;
     
     private AnimationState currentState;
 
     private void Update()
     {
+        verticalVelocity.AddSample(playerController.Velocity.y, Time.deltaTime);
+        animator.SetFloat("VerticalVelocity", verticalVelocity);
+        
+        animator.SetBool("Grounded", playerController.GroundCheck());
+        
         horizontalVelocity.AddSample(playerController.Velocity.x, Time.deltaTime);
         if (Mathf.Abs(horizontalVelocity) < 0.01f)
             return;
