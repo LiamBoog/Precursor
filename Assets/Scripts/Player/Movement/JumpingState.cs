128d4fc008014a3d0469413e3b71793a61d4cf63
Liam Boog <mothercow5@gmail.com>
2025-01-14 17:56:03 -0500

    fix sprite pivots again

--- Assets/Scripts/Player/Movement/JumpingState.cs
+++ Assets/Scripts/Player/Movement/JumpingState.cs
@@ -29,7 +29,6 @@ public class JumpingState : MovementState
         onFirstUpdate = (ref KinematicState<Vector2> kinematics) =>
         {
             onFirstUpdate = null;
-            Debug.Log(parameters.JumpVelocity);
             kinematics.velocity.y = parameters.JumpVelocity;
         };
         player.JumpBuffer.Flush();
